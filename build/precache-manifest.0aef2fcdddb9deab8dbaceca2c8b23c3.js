aac82cd4394fce51e1851472a42df94c2935408f
Trần Quang Vũ <tranquangvu.pis@gmail.com>
2020-10-29 16:33:52 +0700

    frst commit

---
+++ build/precache-manifest.0aef2fcdddb9deab8dbaceca2c8b23c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60bec30470ae9fd1bbb9a214afc2dd29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92c4b6758c110e3499ae",
+    "url": "/static/css/main.fa630228.chunk.css"
+  },
+  {
+    "revision": "c2156a3806aafba54f29",
+    "url": "/static/js/2.c66ee90e.chunk.js"
+  },
+  {
+    "revision": "3bf6c32fa60fa0f7b41615b984399950",
+    "url": "/static/js/2.c66ee90e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92c4b6758c110e3499ae",
+    "url": "/static/js/main.9dc7171b.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
